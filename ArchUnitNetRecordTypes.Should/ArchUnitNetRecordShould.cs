15d572adb0f53659879e425a55730727fbc203d5
Stenger, Michael <michael.stenger@sivantos.com>
2021-05-04 10:58:24 +0200

    adding non-abstract class and copy test

--- ArchUnitNetRecordTypes.Should/ArchUnitNetRecordShould.cs
+++ ArchUnitNetRecordTypes.Should/ArchUnitNetRecordShould.cs
@@ -6,6 +6,21 @@ namespace ArchUnitNetRecordTypes.Should
     public class ArchUnitNetRecordShould
     {
         [Fact]
+        public void CreateCopy()
+        {
+            const string serialNumber = "abc123";
+            const string newName = "NewName";
+            const string oldName = "OldName";
+
+            var oldProduct = new SomeProduct(serialNumber, oldName);
+            var newProduct = oldProduct.CopyWithNewName(newName);
+
+            Assert.Equal(oldName, oldProduct.Name);
+            Assert.Equal(newName, newProduct.Name);
+            Assert.Equal(serialNumber, newProduct.SerialNumber);
+        }
+
+        [Fact]
         public void WorkWithRecordTypes()
         {
             var rootAssembly = typeof(ProductIdentityBase).Assembly;
--- ArchUnitNetRecordTypes/ProductIdentity.cs
+++ ArchUnitNetRecordTypes/ProductIdentity.cs
@@ -1,4 +1,6 @@
-﻿namespace ArchUnitNetRecordTypes
+﻿using System;
+
+namespace ArchUnitNetRecordTypes
 {
     public abstract record ProductIdentityBase(string SerialNumber, string Name)
     {
@@ -6,5 +8,12 @@
         {
             return this with { Name = name };
         }
+
+        public void HarmlessMethod()
+        {
+            Console.WriteLine(this);
+        }
     }
+
+    public record SomeProduct(string SerialNumber, string Name) : ProductIdentityBase(SerialNumber, Name);
 }
